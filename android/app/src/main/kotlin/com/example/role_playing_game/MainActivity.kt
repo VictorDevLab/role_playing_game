18de581bdc9b0df257108ac6891cbca6b30af804
Victor koome <koomev786@gmail.com>
2026-01-16 23:04:37 +0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/role_playing_game/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.role_playing_game
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:role_playing_game/screens/home/home.dart';
+
+void main() {
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
+}
