1cbdc6b5d131371732f0594916e689657aa3372f
Victor koome <koomev786@gmail.com>
2026-02-12 22:55:28 +0300

    firebase SDK setup

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ import 'package:role_playing_game/screens/create/create.dart';
 import 'package:role_playing_game/screens/home/home.dart';
 import 'package:role_playing_game/services/character_store.dart';
 import 'package:role_playing_game/theme.dart';
+//firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+// ...
+
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     ChangeNotifierProvider(
       create: (context) => CharacterStore(),
