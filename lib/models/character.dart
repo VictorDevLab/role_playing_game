c31c65ee2e3ec53836ea5a5fc6be00adc0e07fa6
Victor koome <koomev786@gmail.com>
2026-02-16 07:50:42 +0300

    serialize & deserialize character data for firestore

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:role_playing_game/models/skill.dart';
 import 'package:role_playing_game/models/stats.dart';
 import 'package:role_playing_game/models/vocation.dart';
@@ -31,5 +32,49 @@ class Character with Stats {
   void updateSkill(Skill skill) {
     skills.clear();
     skills.add(skill);
+  }
+
+  //character to firestore(Saving character as a map)//(Serialization)
+  Map<String, dynamic> toFireStore() {
+    return {
+      'name': name,
+      'slogan': slogan,
+      'isFav': isFav,
+      'vocation': vocation.toString(),
+      'skills': skills.map((skill) => skill.id).toList(),
+      'stats': statsAsMap,
+      'points': points,
+    };
+  }
+
+  //character from firestore(deserialize)
+  factory Character.fromFireStore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    //1st) Get data form the snapshot;
+    final data = snapshot.data()!;
+    //2nd)Make character instance
+    Character character = Character(
+      id: snapshot.id,
+      name: data['name'],
+      slogan: data['slogan'],
+      vocation: Vocation.values.firstWhere(
+        (v) => v.toString() == data['vocation'],
+      ),
+    );
+    //update skills
+    for (String id in data['skills']) {
+      Skill skill = allSkills.firstWhere((skill) => skill.id == id);
+      character.updateSkill(skill);
+    }
+    //set is fav
+    if (data['isFav'] == true) {
+      character.toggleIsFav();
+    }
+    //assign stats and points
+    character.setStats(points: data['points'], stats: data['stats']);
+
+    return character;
   }
 }
