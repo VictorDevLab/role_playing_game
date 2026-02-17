c8ebecc990edca0b2777df99bb707fa78f265fb9
Victor koome <koomev786@gmail.com>
2026-02-17 09:18:37 +0300

    converters- process data to and from firestore

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -4,6 +4,7 @@ import 'package:role_playing_game/models/character.dart';
 class FirestoreService {
   static final ref = FirebaseFirestore.instance
       .collection("characters")
+      //to process data to and from firestore
       .withConverter(
         fromFirestore: Character.fromFireStore,
         toFirestore: (Character c, _) => c.toFireStore(),
